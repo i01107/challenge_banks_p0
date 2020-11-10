dd00588d1e07b637f19faa6ddb1ce54a777c8c82
Fiqi Fitransyah <ffitransyah@hacktiv8.com>
2020-11-10 11:55:40 +0700

    update 20201110

---
+++ hard-array-susun-pesan.js
@@ -0,0 +1,24 @@
+/*
+Buatlah fungsi dengan nama susunPesan, dengan parameter input berupa array 2 dimensi. Setiap elemen array akan berupa array yang berisi kata dan posisi nya. Tugas fungsi ini adalah untuk menghasilkan kalimat sesuai dengan posisi kata. Silakan menambahkan contoh soal lain jika merasa perlu
+
+Contoh :
+
+[
+  ['besok', 4],
+  ['kita', 1],
+  ['akan', 2],
+  ['menyerang', 3],
+  ['pagi', 5],
+]
+
+outputnya : 'kita akan menyerang besok pagi'
+
+
+[
+  ['granat', 3],
+  ['musuh', 1],
+  ['membawa', 2],
+]
+
+outputnya : 'musuh membawa granat'
+*/
---
+++ medium-object-kompilasi-jurusan.js
@@ -0,0 +1,21 @@
+/*
+Buatlah fungsi dengan nama kompilasi, dengan parameter input array 2 dimensi seperti pada contoh. Fungsi bertugas untuk membuat rekap data dalam bentuk object, dimana semua data akan dikelompokkan berdasarkan jurusan. Bentukan output dapat dilihat pada contoh
+
+Contoh :
+
+[
+  ['Biologi', 'Jonas'],
+  ['Fisika', 'Ulrich'],
+  ['Akuntansi', 'Hannah'],
+  ['Biologi', 'Barbosz'],
+  ['Fisika', 'Claudia'],
+  ['Biologi', 'Edmund'],
+]
+
+output : 
+{
+  Biologi: ['Jonas', 'Barbosz', 'Edmund'],
+  Fisika: ['Ulrich', 'Claudia'],
+  Akuntansi: ['Hannah']
+}
+*/
