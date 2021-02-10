3f6183f2c9fe85c02bec0b875d01be9ff376dcad
Fiqi Fitransyah <ffitransyah@hacktiv8.com>
2021-02-10 15:20:52 +0700

    Add flagging for bubble sort

---
+++ hard-flagging-bubble-sort.js
@@ -0,0 +1,12 @@
+/*
+Buatlah sebuah function bernama myBubbleSort yang akan menerima sebuah array of number. Implementasikan algoritma bubble sort yang di improve dengan menggunakan flagging. Flagging dipakai untuk menentukan apakah pada iterasi terakhir ada dilakukan value switching ( penukaran nilai ). Jika tidak ada, maka tidak perlu melakukan iterasi lagi setelahnya, jika masih ada
+*/
+
+function myBubbleSort(arr) {
+  // your code here
+}
+
+let input = [1, 2, 3, 56, 2, 55, 7, 4, 77, 4, 3, 2, 1]
+
+console.log(myBubbleSort(input))
+// [1, 1, 2,  2,  2, 3, 3, 4, 4, 55, 56, 7, 77]
